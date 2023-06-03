3e8eb295b52411a1922198a279fafb545aaa414e
Devon Gulley <devongulley1602@gmail.com>
2023-06-03 14:54:27 -0400

    Updated back end and implemented Barnes Hutt structure with forces algorithm in bodies.cpp pending completion

--- body.h
+++ body.h
@@ -1,31 +1,130 @@
 #include <iostream>
+#include <cmath>
+#include<array>
+#include <bits/stdc++.h>
+using namespace std;
 
 class Body {
 
+	protected:
+		double main, domain;
 	public:
 		//Properties of the object
-		double mass,charge,position[3],velocity[3];
-		
+		double startingMass, mass,position[3],velocity[3];
+		Body *children[8];
+		static double cutoffDistance;
+		bool isExternal;
+
+
 		//Constructor
-		Body (double _mass, double _position[3],double _velocity[3], double _charge = 0) {
-			mass = _mass;
-			charge = _charge;
-				for (int i = 0 ; i<3;i++){
-					position[i] = _position[i];
-					velocity[i] = _velocity[i];
-				}
+		Body (double _mass=0, double _position[3]={},double _velocity[3]={}) {
+			initializeBody(_mass,_position,_velocity);		
 		}
+		
+		void initializeBody (double _mass, double _position[3],double _velocity[3]) {
+			cutoffDistance = 0.0001;
+			domain = 1.000000 ;//the side length of the default quadrant (i.e. 1.0)
+			startingMass = _mass;
+			mass = _mass;
+			for (int i = 0 ; i<3;i++){
+				position[i] = _position[i];
+				velocity[i] = _velocity[i];
+			}
+			
+			//Until assigned, the children of a particular body mustn't exist in memory and thus the body is external on the Barnes-Hutt tree
+			for(int i = 0 ; i < 8 ; i++){
+				children[i] = NULL;
+			}
+			isExternal = true;
+		}
+
+
+
 
 		double kineticEngergy () {
 			double v_squared = 0;
 			for (int i = 0; i<3;i++){
-				v_squared += velocity[i]*velocity[i];
+				v_squared += (velocity[i])*(velocity[i]);
 			}
 			return 0.5 * mass * v_squared;
 		}
 
+		
+
+
+
+		double distanceSquaredTo(Body b){
+			double R_sq = 0;
+			//Computing Pythagoras' theorem for each axis
+			for (int i = 0;i<3;i++){
+				R_sq += pow(2,position[i] - b.position[i]);
+			}
+			return R_sq;
+		}
+
+		double Domain(){
+			return domain;
+		}
+
+		
+		//The centre of mass is calculated from: (Sum of each mass * each position vector)/ total mass
+		//As soon as a body has child nodes, it is no longer a body, but a representation of the centre of mass of its children in space, therefore we must
+		void updateCentreOfMass(){
+			double com[3] = {position[0],position[1],position[2]};
+			
+			if(!isExternal){
+				//the sum of each mass * each position vector
+				for( int i = 0; i<8; i++){
+					if(children[i] != NULL){
+						for (int j = 0 ; j<3; j++){
+							com[j] += ((*children[i]).mass)*(*children[i]).position[j];		
+						}
+					}
+				}
+			
+				//divided by the overall mass
+				for (int i = 0 ; i<3; i++){
+					com[i] /= mass;
+					position[i] = com[i];
+				}
+			}
+
+
+		}
+
+
+		//Place an input force to be modified by reference directly (i.e usage in the system could look like myBody.forceOn(otherBody,theForceUsedForCalculationsBetweenThem) )
+		void forceOn(Body b,double (&F)[3]){//a proportional force which the program driver can scale appropriately
+
+			for (int i = 0; i<3; i++){
+				F[i] =(b.position[i]-position[i])*mass*b.mass/(distanceSquaredTo(b)*pow(0.5,distanceSquaredTo(b)));//the force projection onto each axis
+			}
+		}
+		
+
+		//Places the node into the octant in the next level down
+		double nextOctant () {
+			//Divide the domain in half on each axis, positions outside this will be placed in the appropriate octant
+			domain *= 0.5;
+
+			int octantIndex = 0;
+
+			//In each dimension i, we determine the octant in which the particle is in based off the size of the domain
+			for (int i = 0; i<3; i++){
+				double relativePosition = (position[i])/domain;
+				octantIndex += (relativePosition>domain) *pow(2,i); //the octant from 0 to 7 corresponding to if the relativePosition is greater or less than 1 on half of the new domain  	
+			}
+			return octantIndex;
+		}
+		
+
+		void resetTo0th () {
+			initializeBody(startingMass,position,velocity);
+		}
+
+
 		void print() {
-			std::cout << "Mass: "<< mass << "\nPosition: ("<<position[0]<<","<< position[1]<< "," << position[2] << ")\nVelocity: ("<<velocity[0]<<","<< velocity[1]<< "," << velocity[2] << ")";
+			std::cout << "Mass: "<< mass << "\nPosition: ("<<position[0]<<","<< position[1]<< "," << position[2] << ")\nVelocity: ("<<velocity[0]<<","<< velocity[1]<< "," << velocity[2] << ")\n\n";
 
 		}
 };
--- system.cpp
+++ system.cpp
@@ -1,47 +1,82 @@
 #include "body.h"
 
-//A Node can either be  a Body, or represent the space of the octants around it
-class Node {
-	public:
-	Body value;
-	Node* octant[8];
-	Node* parent;
-	
-	double centreOfMass[3], totalMass;
-
-	void print () {
-		value.print();
-	}
-
-};
-
 class System {
-	Node head;
+
+	public:
+		Body headNode;
+		double theta;
+
+
 
 	/*
 	  Insertion of a new Body into octant 0 to 7
-		- If currentNode is an external Node (has not filled in any octants and thus is itself a body) we insert the newNode and the currentNode value into octant(s) recursively
-		- If currentNode is an internal Node (has filled in any octants and thus is not itself a real body) we insert the new Node into one of the octants
+		- If the node does not contain a body, put one there
+		- If node is an external (has not filled in any octants and thus is itself a physical body) we insert the node and the body into octant(s) recursively
+		- If node is an internal (has filled in any octants and thus is not itself a center for the space) we insert the body into one of the octants
+/Body
 		- We then update the mass of the currentNode 
-	*/
-
-	void insert(Node newNode,Node currentNode,int octant){
-
-		//Checking if the currentNode is external or not 
-		bool external = false;
-		for(int i = 0; i<8; i++){ if(currentNode.octant[i]){external = true;}}
 		
-		if(external){
-			//insert the body		 
-			
-			//We calculate the centre of mass using each nonempty octant of the node
-			double totalMass=0;
-			for(int i = 0; i<8;i++){
+		This will create the 3d Barnes-Hutt Tree
+	*/
+		Body *addBarnesHutt(Body* body, Body* node = NULL){
+				//We must be clever with the use of pointers, for standardisation, all bodies will be referred by address
+				Body* newNode = NULL;
+
+				//Insertion of the body into the newNode if the node doesn't exist
+				if(node == NULL){
+					newNode=body;
+				}else{//the node exists
+
+					//Determine if the node is internal or external, this is the only case in the program since instantiation of the body where this value should change
+					for(int i = 0 ; i< 8 ; i++){
+						if((*node).children[i] != NULL) {(*node).isExternal = false;}
+					}
+
+					
+					if((*node).isExternal){//ie if it has no children  
+					  //then there are two bodies conflicting for the same node, they must be added as children to the current 
+						*newNode = Body((*node).startingMass,(*node).position,(*node).velocity) ;//copy of the original body node placed here but without its children
+						int newNodeOctant = (*node).nextOctant();
+						(*node).isExternal = false;//we only need set a node to internal if it has children
+						(*node).children[newNodeOctant] = addBarnesHutt(newNode,(*node).children[newNodeOctant]);
+						(*node).updateCentreOfMass();
+					}else{
+						newNode = node;
+					}
+
+					(*newNode).mass += (*body).mass;//the mass of the system below this node increases by the body being added
+					int octant = (*body).nextOctant();//determine where the octant that the new body at this depth should be inserted
+					(*newNode).children[octant] = addBarnesHutt(body,(*node).children[octant]); //adds the body to be a child of the node
+
+				}	
 				
-				if(currentNode.octant[i]){//if a Node exists on that index
-					totalMass += (*currentNode.octant[i]).value.mass;
-				}
+				return newNode;
+		};
+	
+
+		//recursively move down the tree and for each body, sum together its relative attractions to each neighbour (which may be internal or external)
+		void calculateForces(Body body, Body node,double (&F)[3]){
+			double F_ab[3] = {0,0,0};
+
+			//Calculate the force exerted by the node and store the result by reference in F_ab so long as it's external 
+			if(node.isExternal || (node.Domain()*node.Domain()/node.distanceSquaredTo(body)) < theta ){
+				node.forceOn(body,F_ab);
+			}else{
+
+
+
+			}
+
+			
+			for (int i = 0 ; i < 3 ; i++){
+				F[i] += F_ab[i];
 			}
 		}
-	}
+
+		void Verlet(){
+
+		}
+	
+	
+
 };
