3dfceeb885a2c4328c0f0061f92a262110a5d805
Devon Gulley <devongulley1602@gmail.com>
2023-06-05 21:21:08 -0400

    Documented system and body better, added internal logic for each snapshot of the Verlet algorithm. Cleanded up naming conventions.

--- body.h
+++ body.h
@@ -1,29 +1,96 @@
+/* body.h
+ *
+ * Author: Devon Gulley
+ *
+ * Completion Date: 05-06-2023
+ *
+ * A node data element to represent a physical point mass in a 3-d space
+ * Can also be used to represent a centre of mass for its subnodes to be implemented in <system.h>::System
+ *
+ *
+ * Constructor Body:
+ * 	Creates a point object based off of an initial mass, position, and velocity
+ *
+ *
+ * Public Member Functions:
+ * 
+ * initialiseBody:
+ * 	To be called by the constructor, or each time the Barnes-Hutt tree is rebuilt to unstack the body from the tree
+ * 	Accepts the mass, new position, new velocity,unlinks any subnodes, and resets the domain of the Body
+ *
+ * kineticEnergy:
+ * 	Returns the value of the kinetic energy based off of the current velocity of the body
+ *
+ * distanceSquaredTo:
+ * 	Accepts another body as input
+ * 	Returns the square of the Euclidean distance in 3d space between the two bodies
+ *
+ * Domain:
+ * 	Returns the value of the current domain size which a body occupies
+ * 
+ * updateCentreOfMass:
+ *  Changes the position of an internal node to match the centre of mass of all its subnodes
+ *
+ * forceOn:
+ *  Calculates the force between a node and another body
+ *  Accepts the body, and the force being modified by reference 
+ *
+ * nextOctant:
+ * 	Returns the domain size of the octant that a given body is in
+ *
+ * resetTo0th:
+ * 	Sets all values of a body to its initial condition as defined in initialiseBody
+ *
+ * print:
+ * Sends key statistics to std::cout regarding properties of the body
+ */
+
+
 #include <iostream>
 #include <cmath>
-#include<array>
-#include <bits/stdc++.h>
 using namespace std;
 
 class Body {
 
 	protected:
-		double main, domain;
+		//The width of the octant in whcih a particular body occupies
+		double domain;
 	public:
 		//Properties of the object
 		double startingMass, mass,position[3],velocity[3];
-		Body *children[8];
-		static double cutoffDistance;
-		bool isExternal;
+		Body *subnode[8];//subnodes on the Barnes-Hutt tree structure for each octant
+		static double cutoffDistance;//proximity in which the force between bodies is set 0 to avoid singularities
+		bool isExternal; //true when the body is not a representation of other bodies in space
 
 
-		//Constructor
+		/*
+		 * Constructor for the Body class
+		 *
+		 * The initial traits of the body being initialised
+		 *
+		 * @params _mass
+		 * @params _position[3]
+		 * @params _velocity[3]
+		 *
+		 */
 		Body (double _mass=0, double _position[3]={},double _velocity[3]={}) {
-			initializeBody(_mass,_position,_velocity);		
+			initialiseBody(_mass,_position,_velocity);		
 		}
-		
-		void initializeBody (double _mass, double _position[3],double _velocity[3]) {
+
+
+   	/*
+		 * initialiseBody:
+		 *
+		 * @params _mass
+		 * @params _position[3]
+		 * @params _velocity[3]
+     *
+		 * Sets the initial values of the body while maintaining its current state in the sytem such
+		 * Should be called each time the force is updated or when the system tree is rebuilt
+		 */
+		void initialiseBody (double _mass, double _position[3],double _velocity[3]) {
 			cutoffDistance = 0.0001;
-			domain = 1.000000 ;//the side length of the default quadrant (i.e. 1.0)
+			domain = 1.000000 ;//the side length of the default octant (i.e. 1.0)
 			startingMass = _mass;
 			mass = _mass;
 			for (int i = 0 ; i<3;i++){
@@ -31,16 +98,18 @@ class Body {
 				velocity[i] = _velocity[i];
 			}
 			
-			//Until assigned, the children of a particular body mustn't exist in memory and thus the body is external on the Barnes-Hutt tree
+			//On initialisation/reset of the system tree, all subnodes of a given body are unlinked in memory
 			for(int i = 0 ; i < 8 ; i++){
-				children[i] = NULL;
+				subnode[i] = NULL;
 			}
 			isExternal = true;
 		}
 
 
-
-
+		/*
+		 * kineticEnergy:
+		 * 	Returns the non-relativistic kinetic energy of the body in non-scaled units
+		 */
 		double kineticEngergy () {
 			double v_squared = 0;
 			for (int i = 0; i<3;i++){
@@ -49,10 +118,13 @@ class Body {
 			return 0.5 * mass * v_squared;
 		}
 
-		
-
-
-
+	
+		/* 
+		 * distanceSquaredTo:
+		 * 	Returns the Euclidean distance to another body b
+		 *
+		 * @params b
+		 */
 		double distanceSquaredTo(Body b){
 			double R_sq = 0;
 			//Computing Pythagoras' theorem for each axis
@@ -62,22 +134,30 @@ class Body {
 			return R_sq;
 		}
 
+		
+	  // @property Domain 
 		double Domain(){
 			return domain;
 		}
 
 		
-		//The centre of mass is calculated from: (Sum of each mass * each position vector)/ total mass
-		//As soon as a body has child nodes, it is no longer a body, but a representation of the centre of mass of its children in space, therefore we must
+/*
+ * updateCentreOfMass:
+ * 
+ * Returns the calculation for the centre of mass of all of a node's subnodes 
+ * The centre of mass is calculated from: (Sum of each mass * each position vector)/ total mass
+ * As soon as a body has subnodes, it is no longer a body, but a representation of the centre of mass of its subnode in space
+ *
+ */ 
 		void updateCentreOfMass(){
 			double com[3] = {position[0],position[1],position[2]};
 			
 			if(!isExternal){
 				//the sum of each mass * each position vector
 				for( int i = 0; i<8; i++){
-					if(children[i] != NULL){
+					if(subnode[i] != NULL){
 						for (int j = 0 ; j<3; j++){
-							com[j] += ((*children[i]).mass)*(*children[i]).position[j];		
+							com[j] += ((*subnode[i]).mass)*(*subnode[i]).position[j];		
 						}
 					}
 				}
@@ -93,16 +173,29 @@ class Body {
 		}
 
 
-		//Place an input force to be modified by reference directly (i.e usage in the system could look like myBody.forceOn(otherBody,theForceUsedForCalculationsBetweenThem) )
+		/*
+		 * forceOn:
+		 * 	Calculates the force between this node and Body b based off of law of gravitation for point masses
+		 * 	Stores the result of the force from this into the input parameter F[3] by reference
+		 * 	The order of the subtraction of their positions designates the direction as pointing away from b
+		 *
+		 * @params b
+		 * @params F[3]	
+		 */ 
 		void forceOn(Body b,double (&F)[3]){//a proportional force which the program driver can scale appropriately
-
 			for (int i = 0; i<3; i++){
-				F[i] =(b.position[i]-position[i])*mass*b.mass/(distanceSquaredTo(b)*pow(0.5,distanceSquaredTo(b)));//the force projection onto each axis
+				//the force projection onto each axis
+				F[i] =(position[i]-b.position[i])*mass*b.mass/(distanceSquaredTo(b)*pow(0.5,distanceSquaredTo(b)));
 			}
 		}
 		
 
-		//Places the node into the octant in the next level down
+		/* 
+		 * nextOctant:
+		 * 	Determines the position of the node into the octant the next level down when subdividing along the system tree
+		 * 	Divides the domain of the body in half to set it as a subnode of its current
+		 * 	Returns the index of the appropriate octant as octantIndex
+		 */
 		double nextOctant () {
 			//Divide the domain in half on each axis, positions outside this will be placed in the appropriate octant
 			domain *= 0.5;
@@ -117,12 +210,20 @@ class Body {
 			return octantIndex;
 		}
 		
-
+		
+		/*
+		 * resetTo0th:
+		 * 	Resets the body to its initial conditions with its updated position, and velocity by calling initialiseBody 
+		 */
 		void resetTo0th () {
-			initializeBody(startingMass,position,velocity);
+			initialiseBody(startingMass,position,velocity);
 		}
 
 
+		/*
+		 * print:
+		 *  Sends to std::cout a printout of properties including mass, position[3] values, and velocity[3] values 
+		 */
 		void print() {
 			std::cout << "Mass: "<< mass << "\nPosition: ("<<position[0]<<","<< position[1]<< "," << position[2] << ")\nVelocity: ("<<velocity[0]<<","<< velocity[1]<< "," << velocity[2] << ")\n\n";
 
--- system.cpp
+++ system.cpp
@@ -1,82 +1,122 @@
+/* system.h
+ *
+ * Author: Devon Gulley
+ *
+ * Completion Date: 05-06-2023
+ *
+ * To assemble the system of <body.h>::Body into a Barnes-Hutt implementation using the law of gravitation between point masses.
+ * Clusters close masses as a body with its centre of mass visible to distant bodies to reduce computation pairs.
+ *
+ * Public Member Functions:
+ *
+ * 	addBarnesHutt:
+ * 		Builds the Barnes-Hutt tree one body at a time and clusters bodies based off of their properties in body.h
+ * 		Accepts the body to insert, as well as along which node it begins the search for insertion into.
+ * 		Returns the new node it has added to the sytsem
+ * 		
+ *	calculateForces:
+ *		For one instant in time, calculates the net force acting on each body in the system
+ *		Accepts the body in which it is calculating the net force on,and the top node to recurse down for
+ *		Accepts and returns by reference the net force F on the body
+ *
+ */
+
+
+
 #include "body.h"
 
 class System {
 
 	public:
 		Body headNode;
-		double theta;
+
+		//The Barnes-Hutt theta criterion set by the system (typically 0.5) -> the ratio of the domain of an internal node to the distance it is from a body 
+		double theta;	
 
 
 
-	/*
-	  Insertion of a new Body into octant 0 to 7
-		- If the node does not contain a body, put one there
-		- If node is an external (has not filled in any octants and thus is itself a physical body) we insert the node and the body into octant(s) recursively
-		- If node is an internal (has filled in any octants and thus is not itself a center for the space) we insert the body into one of the octants
-/Body
-		- We then update the mass of the currentNode 
-		
-		This will create the 3d Barnes-Hutt Tree
-	*/
+		/*
+	 	 * Builds the Barnes-Hutt tree to represent point body clusters
+		 *
+		 * @param body The body being inserted into the Barnes-Hutt tree.
+		 * @param node The location in the tree where the body is intended to be inserted (which is itself of type Body as well.)
+	   *
+	   * Insertion of a new Body into octant 0 to 7
+	   * - If the node does not contain a body, put one there
+	   * - If node is an external (has not filled in any octants and thus is itself a physical body) we insert the node and the body into octant(s) recursively
+	   * - If node is an internal (has filled in any octants and thus is not itself a center for the space) we insert the body into one of the octants/Body
+	   * - We then update the mass of the currentNode 
+	   *	
+	   *	This will create the 3d Barnes-Hutt Tree each calculation of the forces during an iteration of the Verlet algorithm.
+	   */
+	
 		Body *addBarnesHutt(Body* body, Body* node = NULL){
-				//We must be clever with the use of pointers, for standardisation, all bodies will be referred by address
 				Body* newNode = NULL;
 
 				//Insertion of the body into the newNode if the node doesn't exist
 				if(node == NULL){
 					newNode=body;
-				}else{//the node exists
-
-					//Determine if the node is internal or external, this is the only case in the program since instantiation of the body where this value should change
+				}else{ //the node exists already
+					
+					//Determine if the node is internal or external, this is the only case in the program since instantiation, addition of a subnode, or a reset of the Barnes Hutt tree where this value should change
 					for(int i = 0 ; i< 8 ; i++){
-						if((*node).children[i] != NULL) {(*node).isExternal = false;}
+						if((*node).subnode[i] != NULL) {(*node).isExternal = false;}
 					}
 
-					
-					if((*node).isExternal){//ie if it has no children  
-					  //then there are two bodies conflicting for the same node, they must be added as children to the current 
-						*newNode = Body((*node).startingMass,(*node).position,(*node).velocity) ;//copy of the original body node placed here but without its children
+					//If it external (has no subnode) then there are two bodies conflicting for the same node, they must be added as subnode to the current 
+					if((*node).isExternal){
+						//copy of the original body node placed here but without its subnodes
+						*newNode = Body((*node).startingMass,(*node).position,(*node).velocity) ;
 						int newNodeOctant = (*node).nextOctant();
-						(*node).isExternal = false;//we only need set a node to internal if it has children
-						(*node).children[newNodeOctant] = addBarnesHutt(newNode,(*node).children[newNodeOctant]);
-						(*node).updateCentreOfMass();
-					}else{
+						(*node).isExternal = false;//we only need set a node to internal if it has any subnodes
+						(*node).subnode[newNodeOctant] = addBarnesHutt(newNode,(*node).subnode[newNodeOctant]);
+						(*node).updateCentreOfMass(); 
+					}else{ //it is internal (has a subnode) so the newNode can become the node which was passed into the function (base case) 
 						newNode = node;
 					}
 
 					(*newNode).mass += (*body).mass;//the mass of the system below this node increases by the body being added
 					int octant = (*body).nextOctant();//determine where the octant that the new body at this depth should be inserted
-					(*newNode).children[octant] = addBarnesHutt(body,(*node).children[octant]); //adds the body to be a child of the node
+					(*newNode).subnode[octant] = addBarnesHutt(body,(*node).subnode[octant]); //adds the body to be a subnode of the node
 
 				}	
 				
 				return newNode;
 		};
-	
+		
+		/*
+		 * Calculation of the forces of all subnodes starting at node onto the body
+		 *
+		 * @param body The body whose forces are being calculated for.
+		 * @param node The node whose subnodes contribute to the forces acting on body.
+		 * @param F    The reference value of the input force being changed through the forces algorithm.
+		 *
+		 * Recursively moves down the tree and sums together the force on a given body contributed by every relevant node in the system.
+		 * - For nodes that are external, the force it exerts on the body will be summed and contributed
+		 * - For nodes that are internal but satisfy the theta criterion and are sufficiently far from a given body, the same will occur
+		 * - Nodes that are internal, but too close to the body in question, its subnodes will each recurse on this function to contribute to the force
+		 */
 
-		//recursively move down the tree and for each body, sum together its relative attractions to each neighbour (which may be internal or external)
+
 		void calculateForces(Body body, Body node,double (&F)[3]){
+			
+			//Calculate the force exerted below the node and store the result by reference in F_ab 
 			double F_ab[3] = {0,0,0};
 
-			//Calculate the force exerted by the node and store the result by reference in F_ab so long as it's external 
-			if(node.isExternal || (node.Domain()*node.Domain()/node.distanceSquaredTo(body)) < theta ){
+			//The node's position can be used if it's external OR it's internal but sufficiently far from the body (the theta criterion)
+			if(node.isExternal ||( (node.Domain()*node.Domain()< theta*node.distanceSquaredTo(body)) && &node!=&headNode) ){
 				node.forceOn(body,F_ab);
-			}else{
-
-
-
+			}else{//The default recursive body force pair computation
+				for (int i = 0 ; i < 8 ; i++){
+					if (node.subnode[i] != NULL){//if anything can be parallelised easily, it's this
+						(*node.subnode[i]).forceOn(body,F_ab);//recursively determine the contribution of the force from each subnode onto the body
+					}
+				}
 			}
 
-			
+			//To return the sum of the forces from each contribution
 			for (int i = 0 ; i < 3 ; i++){
 				F[i] += F_ab[i];
 			}
 		}
-
-		void Verlet(){
-
-		}
-	
-	
-
 };
